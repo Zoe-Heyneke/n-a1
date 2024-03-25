a57fadee65c7960bd6994644556351014b22229f
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-03-25 22:07:09 +0200

    For loop for each number that he user chose, the user will be asked to enter the name, quantity, and unit of measurement of the ingredient

--- Program.cs
+++ Program.cs
@@ -13,6 +13,16 @@
         Console.WriteLine("Enter the number of ingredients that are in this recipe:");
         int numIng= Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("The number of ingredients you chose are: " + numIng);
+
+        for (int i = 0; i < numIng; i++) 
+        {
+            Console.WriteLine("Enter the name of the ingredient:");
+            string nameIng = Console.ReadLine();
+            Console.WriteLine("Enter the quantity of the ingredient (amount):");
+            int quantityIng = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the unit of measurement of the ingredient (cup, tablespoon etc.)");
+            string unitMea = Console.ReadLine();
+        }
     }
 
 }
